af8dc4915ad1777a8c3e2d31f399e5c6bc7eda22
Tsx <zachem08@icloud.com>
2026-03-24 15:19:28 +0500

    initial commit

---
+++ build/CMakeFiles/octolink.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/octolink.dir/link.d"
+  "CMakeFiles/octolink.dir/src/server.cpp.o"
+  "CMakeFiles/octolink.dir/src/server.cpp.o.d"
+  "octolink"
+  "octolink.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/octolink.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ server.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace std;
+using boost::asio::ip::tcp;
+
+int main(){
+	cout << "i just started" << endl;
+	return 0;
+}
